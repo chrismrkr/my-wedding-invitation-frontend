18718bdcad1f84d8b63b1a1e60df856b2f477556
okee <b635032_@daum.net>
2025-07-18 15:42:41 +0900

    edit

--- src/components/MainFrame.vue
+++ src/components/MainFrame.vue
@@ -3,22 +3,10 @@ export default {
     props: ['mainTitle', 'tailTitle'],
     data() {
         return {
-            count : 100,
+            currentScreen : 0,
+            screenCount : 3,
+            condition: 'A'
         }
-    },
-    computed: {
-        computedNow() {
-            return Date.now();
-        }
-    },
-    methods: {
-        increaseCount() {
-            this.count += 1;
-        },
-        methodNow() {
-            return Date.now();
-        }
-
     }
 }
 </script>
@@ -26,20 +14,23 @@ export default {
 <template>
     <div class="background">
         <main class="main">
-            {{ mainTitle }} Count : {{ count }}
-            {{ methodNow() }}
+            <div>
+                {{ mainTitle }}
+            </div>
+            <div class="">
+                <div class="direction left"></div>
+                <div class="direction right"></div>
+            </div>
         </main>
         <tail class="tail">
             {{ tailTitle }}
             main 컴포넌트를 옆으로 전환할 수 있는 거 만들기
-            <button @click="increaseCount">
-                클릭하기
-            </button>
         </tail>
     </div>
 </template>
 
 <style scoped>
+
 
 .background {
     display: flex;
@@ -56,6 +47,11 @@ export default {
     height: 90%;
     margin: 5px;
     background-color: aqua;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
 .tail {
@@ -63,6 +59,26 @@ export default {
     height: 5%;
     margin: 5px;
     background-color:aquamarine;
+}
+
+
+.direction {
+    height: 20px;
+    width: 20px;
+    margin: 30px;
+    background-image: url('@/image/direction-key.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    transition: filter 0.2s ease;
+}
+.direction:hover {
+    filter: brightness(0%) saturate(100%) invert(50%) sepia(100%) saturate(749%) hue-rotate(135deg) brightness(92%) contrast(85%);
+}
+.direction + .right {
+    transform: rotate(180deg);
+}
+.direction + .left {
+
 }
 
 </style>
